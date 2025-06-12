3b3ef8ca8be58cc038d0447b7cc65905e70dc953
Michael Christenson II <christensonmi.ctr@socom.mil>
2025-06-12 07:01:37 -0400

    Moved to warp from rocket

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-#[macro_use]
-extern crate rocket;
+use std::sync::{Arc, Mutex};
+use tokio::sync::broadcast;
+use warp::Filter;
 
-use rocket_cors::CorsOptions;
+mod websockets;
 
-mod websocket;
-
-#[rocket::main]
-async fn main() -> anyhow::Result<()> {
-    let cors = CorsOptions::default().to_cors().unwrap();
-
-    rocket::build()
-        .mount("/ws", routes![websocket::ws_handler,])
-        .attach(cors)
-        .launch()
-        .await?;
-    Ok(())
+#[tokio::main]
+async fn main() {
+    let tx = Arc::new(Mutex::new(broadcast::channel(100).0));
+    let tx_ws = tx.clone();
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .map(move |ws: warp::ws::Ws| {
+            let tx = tx_ws.clone();
+            ws.on_upgrade(move |websocket| websockets::handle_connection(websocket, tx))
+        });
+    warp::serve(ws_route).run(([0, 0, 0, 0], 8000)).await;
 }
--- src/utils/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod backup_codes;
-pub mod passwords;
-pub mod strings;
-pub mod time;
